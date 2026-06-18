750e308da2137c3bef15f8a17e913922a40b5c83
Anchit <anchit1234deshpande@gmail.com>
2026-06-19 00:17:33 +0530

    changed number of frames to be fed=16

--- encoding/temporal_aggregator.py
+++ encoding/temporal_aggregator.py
@@ -20,7 +20,7 @@ class TemporalAggregator(nn.Module):
     def __init__(
         self,
         shared_dim: int = 512,
-        num_frames: int = 8,
+        num_frames: int = 16,
         num_layers: int = 2,
         num_heads: int = 8,
         dropout: float = 0.1,
